9d6e56589c7a95aaf0445c555917417fddfeaf6b
Simon R Jones <simon@studio24.net>
2022-03-13 21:18:53 +0000

    Load response tag event listener is tags enabled in FOSHttpCacheBundle

--- src/DependencyInjection/StrataExtension.php
+++ src/DependencyInjection/StrataExtension.php
@@ -11,7 +11,6 @@ use Symfony\Component\HttpKernel\DependencyInjection\ConfigurableExtension;
 
 class StrataExtension extends ConfigurableExtension
 {
-
     public function loadInternal(array $mergedConfig, ContainerBuilder $container)
     {
         $loader = new XmlFileLoader(
@@ -19,9 +18,15 @@ class StrataExtension extends ConfigurableExtension
             new FileLocator(__DIR__ . '/../Resources/config')
         );
         $loader->load('services.xml');
+        $loader->load('data_collector.xml');
 
         // Pass params to service classes
         $definition = $container->getDefinition('strata.event_subscriber.preview_mode');
         $definition->replaceArgument(0, $mergedConfig['preview_mode']['data_provider']);
+
+        // Load response tagger is loaded via FOSHttpCacheBundle
+        if ($container->has('fos_http_cache.response_tagger')) {
+            $loader->load('response_tagger.xml');
+        }
     }
 }
